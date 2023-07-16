f46bde1d9292d629247330e727adad8ad9798419
Denis Arlakov <darlakov@mail.ru>
2023-07-17 01:59:50 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,96 +1,18 @@
-import { useState } from "react";
-import { Container, Grid, Typography, CssBaseline, Box } from "@mui/material";
+import { CssBaseline } from "@mui/material";
 import "./App.css";
-import InputAmount from "./components/InputAmount";
-import SelectCountry from "./components/SelectCountry";
-import SwitchCurrency from "./components/SwitchCurrency";
-import { useThemeContext } from "./theme/ThemeContextProvider";
 import { ThemeProvider } from "@emotion/react";
 import NightModeToggle from "./components/NightModeToggle";
-import { useContext, useEffect } from "react";
-import { CurrencyContext } from "./context/CurrencyContext";
-import axios from "axios";
+import Home from "./pages/Home/Home";
+import { useThemeContext } from "./theme/ThemeContextProvider";
 
 function App() {
   const { theme } = useThemeContext();
-
-  const {
-    fromCurrency,
-    setFromCurrency,
-    toCurrency,
-    setToCurrency,
-    firstAmount,
-  } = useContext(CurrencyContext)!;
-
-  const [resultCurrency, setResultCurrency] = useState(0);
-  const codeFromCurrency = fromCurrency.split(" ")[0];
-  const codeToCurrency = toCurrency.split(" ")[0];
-
-  useEffect(() => {
-    if (firstAmount) {
-      axios
-        .get("https://api.freecurrencyapi.com/v1/latest", {
-          params: {
-            apikey: "fca_live_R1bjwclp0miUejau7WN75x420xahH198rn9tN5Kr",
-            base_currency: codeFromCurrency,
-            currencies: codeToCurrency,
-          },
-        })
-        .then((response) => {
-          setResultCurrency(response.data.data[codeToCurrency]);
-        })
-        .catch((error) => console.log(error));
-    }
-  }, [firstAmount, fromCurrency, toCurrency]);
-
-  const boxStyles = {
-    textAlign: "center",
-    minHeight: "20rem",
-    borderRadius: 2,
-    background:
-      theme.palette.mode === "dark"
-        ? theme.palette.grey[800]
-        : theme.palette.grey[100],
-    padding: "2rem 2rem",
-    boxShadow: "0px 10px 15px -3px rgba(0,0,0,0.1)",
-    position: "relative",
-  };
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <NightModeToggle />
-      <Container maxWidth="md" sx={boxStyles}>
-        <Typography variant="h5" sx={{ marginBottom: "2rem" }}>
-          Конвертер валют
-        </Typography>
-        <Grid container spacing={2}>
-          <InputAmount />
-          <SelectCountry
-            value={fromCurrency}
-            setValue={setFromCurrency}
-            label="Из"
-          />
-          <SwitchCurrency />
-          <SelectCountry
-            value={toCurrency}
-            setValue={setToCurrency}
-            label="В"
-          />
-        </Grid>
-        {firstAmount ? (
-          <Box sx={{ textAlign: "left", marginTop: "1rem" }}>
-            <Typography variant="h5">
-              {firstAmount} {fromCurrency} =
-            </Typography>
-            <Typography variant="h5" sx={{ marginTop: "5px" }}>
-              {resultCurrency * Number(firstAmount)} {toCurrency}
-            </Typography>
-          </Box>
-        ) : (
-          ""
-        )}
-      </Container>
+      <Home />
     </ThemeProvider>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,86 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import { Container, Grid, Typography, Box } from "@mui/material";
+import { useThemeContext } from "/src/theme/ThemeContextProvider";
+import InputAmount from "/src/components/InputAmount";
+import SelectCountry from "/src/components/SelectCountry";
+import { useState, useEffect, useContext } from "react";
+import { CurrencyContext } from "/src/context/CurrencyContext";
+import axios from "axios";
+import SwitchCurrency from "/src/components/SwitchCurrency";
 
-export default Home
+const Home = () => {
+  const { theme } = useThemeContext();
+
+  const boxStyles = {
+    textAlign: "center",
+    minHeight: "20rem",
+    borderRadius: 2,
+    background:
+      theme.palette.mode === "dark"
+        ? theme.palette.grey[800]
+        : theme.palette.grey[100],
+    padding: "2rem 2rem",
+    boxShadow: "0px 10px 15px -3px rgba(0,0,0,0.1)",
+    position: "relative",
+  };
+
+  const {
+    fromCurrency,
+    setFromCurrency,
+    toCurrency,
+    setToCurrency,
+    firstAmount,
+  } = useContext(CurrencyContext)!;
+
+  const [resultCurrency, setResultCurrency] = useState(0);
+  const codeFromCurrency = fromCurrency.split(" ")[0];
+  const codeToCurrency = toCurrency.split(" ")[0];
+
+  useEffect(() => {
+    if (firstAmount) {
+      axios
+        .get("https://api.freecurrencyapi.com/v1/latest", {
+          params: {
+            apikey: "fca_live_R1bjwclp0miUejau7WN75x420xahH198rn9tN5Kr",
+            base_currency: codeFromCurrency,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          setResultCurrency(response.data.data[codeToCurrency]);
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [firstAmount, fromCurrency, toCurrency]);
+
+  return (
+    <Container maxWidth="md" sx={boxStyles}>
+      <Typography variant="h5" sx={{ marginBottom: "2rem" }}>
+        Конвертер валют
+      </Typography>
+      <Grid container spacing={2}>
+        <InputAmount />
+        <SelectCountry
+          value={fromCurrency}
+          setValue={setFromCurrency}
+          label="Из"
+        />
+        <SwitchCurrency />
+        <SelectCountry value={toCurrency} setValue={setToCurrency} label="В" />
+      </Grid>
+      {firstAmount ? (
+        <Box sx={{ textAlign: "left", marginTop: "1rem" }}>
+          <Typography variant="h5">
+            {firstAmount} {fromCurrency} =
+          </Typography>
+          <Typography variant="h5" sx={{ marginTop: "5px" }}>
+            {resultCurrency * Number(firstAmount)} {toCurrency}
+          </Typography>
+        </Box>
+      ) : (
+        ""
+      )}
+    </Container>
+  );
+};
+
+export default Home;
